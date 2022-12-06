6328e473e27958a7665bdb4974e5ff4d3f21c4c0
Janek Thomaschewski <janek@thomaschewski.dev>
2022-12-06 21:23:14 +0100

    refactor(day06): simplify
    replace enumerate& find with position
    
    works like findIndex in JS.
    removes  need for enumerate to get the index

--- src/day06/shared.rs
+++ src/day06/shared.rs
@@ -1,15 +1,14 @@
 use std::collections::HashSet;
 
 pub fn find_marker(input: &str, num_unique_chars: usize) -> Option<usize> {
-    let marker = input
+    let marker_index = input
         .as_bytes()
         .windows(num_unique_chars)
-        .enumerate()
-        .find(|(_, window)| {
+        .position(|window| {
             // put all chars into a set to check if all n items are distinct
             window.iter().collect::<HashSet<_>>().len() == num_unique_chars
         });
 
-    let marker_index = marker?.0 + num_unique_chars;
+    let marker_index = marker_index? + num_unique_chars;
     Some(marker_index)
 }
