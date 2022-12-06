81192b67756b79eb341f7d7f08eaa5b91a304643
Janek Thomaschewski <janek@thomaschewski.dev>
2022-12-06 19:16:11 +0100

    feat: add support for i32 as well as Options and Results as solution types

--- src/shared.rs
+++ src/shared.rs
@@ -9,6 +9,7 @@ pub enum Part {
 #[derive(Debug)]
 pub enum Solution {
     Number(u32),
+    SignedNumber(i32),
     String(String),
 }
 
@@ -17,6 +18,7 @@ impl Display for Solution {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Solution::Number(n) => write!(f, "{}", n),
+            Solution::SignedNumber(s) => write!(f, "{}", s),
             Solution::String(s) => write!(f, "{}", s),
         }
     }
@@ -45,6 +47,35 @@ impl From<String> for Solution {
 impl From<u32> for Solution {
     fn from(n: u32) -> Self {
         Solution::Number(n)
+    }
+}
+impl From<i32> for Solution {
+    fn from(n: i32) -> Self {
+        Solution::SignedNumber(n)
+    }
+}
+impl From<usize> for Solution {
+    fn from(n: usize) -> Self {
+        Solution::Number(n as u32)
+    }
+}
+// Support any Result<T, _> as Solution, for any T, which implements From<T> (see above, e.g. Result<u32, Err>)
+impl<T, E> From<Result<T, E>> for Solution
+where
+    T: std::convert::Into<Solution>,
+    E: std::fmt::Debug,
+{
+    fn from(n: Result<T, E>) -> Self {
+        n.expect("Got error instead of solution...").into()
+    }
+}
+// Support any Option<T> as Solution, for any T, which implements From<T> (see above, e.g. Option<u32>)
+impl<T> From<Option<T>> for Solution
+where
+    T: std::convert::Into<Solution>,
+{
+    fn from(n: Option<T>) -> Self {
+        n.expect("Got None instead of solution...").into()
     }
 }
 
