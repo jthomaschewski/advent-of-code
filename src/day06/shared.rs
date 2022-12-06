4d05141a62341c5f60ded210f9fd2ec139b82724
Janek Thomaschewski <janek@thomaschewski.dev>
2022-12-06 20:27:19 +0100

    refactor(day06): simplify
    - use "input.as_bytes" to avoid having to collect chars first
    - use collect for hashset conversion (removes need for temp variable)
    - use "?" to replace pattern matching

--- src/day06/shared.rs
+++ src/day06/shared.rs
@@ -1,19 +1,15 @@
 use std::collections::HashSet;
 
 pub fn find_marker(input: &str, num_unique_chars: usize) -> Option<usize> {
-    let vec = input.chars().enumerate().collect::<Vec<(usize, char)>>();
-    let result_window = vec.windows(num_unique_chars).find(|window| {
-        // put all chars into a hash map to check for uniqueness
-        let hs: HashSet<&char> = HashSet::from_iter(window.iter().map(|(_, c)| c));
-        // expect all chars to be unique to be a marker set
-        hs.len() == num_unique_chars
-    });
+    let marker = input
+        .as_bytes()
+        .windows(num_unique_chars)
+        .enumerate()
+        .find(|(_, window)| {
+            // put all chars into a set to check if all n items are distinct
+            window.iter().collect::<HashSet<_>>().len() == num_unique_chars
+        });
 
-    match result_window {
-        Some(window) => {
-            let marker_index = window.last().unwrap().0;
-            Some(marker_index + 1)
-        }
-        None => None,
-    }
+    let marker_index = marker?.0 + num_unique_chars;
+    Some(marker_index)
 }
